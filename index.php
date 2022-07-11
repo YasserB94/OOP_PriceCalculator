f7c5192edf5d17921c72b5aeb5f13e9e964fc6f7
Yasser Bal <yasserbal@icloud.com>
2022-07-11 16:20:25 +0200

    Setup boilerplate for html doc - load controller for calculator page - use env file

--- index.php
+++ index.php
@@ -3,8 +3,20 @@ declare(strict_types=1);
 
 //Head and opening tag for body
 require './Views/head.php';
+//Load ENV FILE INTO SUPERGLOBAL
+//Symfony Dotenv parses .env files to make environment variables stored in them accessible via $_SERVER or $_ENV.
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+use Symfony\Component\Dotenv\Dotenv;
+//Create dotenv object
+$dotenv = new Dotenv();
+//Load .env vars into superglobal $_ENV
+$dotenv->load(__DIR__."/.env");
 
 
-//Footer and closing tag for body/html doc
+//ADD DBCONTROLLER
+require "Controllers/priceCalculatorController.php";
+$controller = new priceCalculatorController();
+$controller->render();
+//Footer and closing tag for body/html
 require './Views/footer.php';
 ?>
